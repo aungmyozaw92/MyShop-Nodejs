798202dd454e367abe8ab63385852288c355ef6a
marathonamz <aungmyozaw@marathonmyanmar.com>
2022-04-15 19:51:03 +0630

    added add-to-cart, checkout process with stripe payment

---
+++ controllers/cart.js
@@ -0,0 +1,133 @@
+const fs = require('fs');
+const path = require('path');
+const stripe = require('stripe')('sk_test_51KoPNdBo4j5AuEEmDwUauT3LqGIzRukUIi9g5vlZcF428WxZZW1UfcxuV3JlWTbBNQzRD1B87ODHWtjBZOBqcnyS00e4VMOZ9F');
+
+const PDFDocument = require('pdfkit');
+
+const Product = require('../models/Product');
+const Order = require('../models/Order');
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    // .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId)
+    .then(product => {
+      return req.user.addToCart(product);
+    })
+    .then(result => {
+      console.log(result);
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+    .removeFromCart(prodId)
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+  req.user
+    .populate('cart.items.productId')
+    // .execPopulate()
+    .then(user => {
+      products = user.cart.items;
+      
+      total = 0;
+      products.forEach(p => {
+        total += p.qty * p.productId.price;
+      });
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map(p => {
+            console.log(p.qty);
+          return {
+            name: p.productId.title,
+            description: p.productId.description,
+            amount: p.productId.price * 100,
+            currency: 'usd',
+            quantity: p.qty
+          };
+        }),
+        success_url: req.protocol + '://' + req.get('host') + '/checkout/success', // => http://localhost:3000
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+      });
+    })
+    .then(session => {
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum: total,
+        sessionId: session.id
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    // .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { qty: i.qty, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
+    })
+    .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,24 @@
 const express = require("express");
 
 const homeController = require("../controllers/home");
+const cartController = require("../controllers/cart");
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 router.get("/", homeController.getIndex);
 router.get("/products", homeController.getProducts);
+
+router.get('/cart', isAuth, cartController.getCart);
+
+router.post('/cart', isAuth, cartController.postCart);
+
+router.post('/cart-delete-item', isAuth, cartController.postCartDeleteProduct);
+
+router.get('/checkout', isAuth, cartController.getCheckout);
+
+router.get('/checkout/success', cartController.getCheckoutSuccess);
+
+router.get('/checkout/cancel', cartController.getCheckout);
 
 module.exports = router;
